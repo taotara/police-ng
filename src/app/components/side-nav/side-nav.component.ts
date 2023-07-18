7de66b60d20eb9fc015e8947086a5d3882394034
taotara <taotara@yahoo.com>
2023-07-18 12:13:26 +0100

    Side nav initiated

--- src/app/components/side-nav/side-nav.component.ts
+++ src/app/components/side-nav/side-nav.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { navbarData } from './nav-data';
 
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
@@ -9,5 +13,20 @@ import { navbarData } from './nav-data';
 export class SideNavComponent {
 
   collapsed = false;
+  screenWidth = 0;
   navData = navbarData;
+
+  @Output() onToggleSideNav: EventEmitter<any> = new EventEmitter();
+  toggleCollapse(): void {
+    this.collapsed = !this.collapsed;
+    this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth})
+  }
+
+  closeSidenav(): void {
+    this.collapsed = false
+    this.onToggleSideNav.emit({
+      collapsed: this.collapsed,
+      screenWidth: this.screenWidth,
+    });
+  }
 }
--- src/app/pages/admin/dashboard/dashboard.component.ts
+++ src/app/pages/admin/dashboard/dashboard.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,4 +11,11 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  isSideNavCollapsed = false;
+  screenWidth = 0;
+
+  onToggleSideNav(data: SideNavToggle): void {
+    this.screenWidth = data.screenWidth;
+    this.isSideNavCollapsed = data.collapsed;
+  }
 }
